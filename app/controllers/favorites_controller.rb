f538076c45c9ec13f2ebb71d83414a3018db3c80
toshiki.isokawa <toshi.99.0225@icloud.com>
2021-04-30 12:11:46 +0000

    modified FavoriteController

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,15 +3,17 @@ class FavoritesController < ApplicationController
   
   def create
     micropost = Micropost.find(params[:micropost_id])
+    user = micropost.user
     current_user.favorite(micropost)
     flash[:success] = 'この投稿をお気に入りに登録しました。'
-    redirect_to User.find(params[:micropost_id])
+    redirect_to user
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
+    user = micropost.user
     current_user.unfavorite(micropost)
     flash[:success] = 'この投稿をお気に入りから解除しました。'
-    redirect_to User.find(params[:micropost_id])
+    redirect_to user
   end
 end
